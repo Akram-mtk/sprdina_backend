af5dbe509e5557db194736ab57eecf6730a380bb
Akram-mtk <matakram872@gmail.com>
2026-05-31 07:36:23 +0100

    added CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://sprdina-b2c5e7.netlify.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
